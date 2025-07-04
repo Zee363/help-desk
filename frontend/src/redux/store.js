ce52b9dafef13f1ba5b5670409dfbb609141be10
Zinhle Nhlapo <zinhle.nhlapo31@gmail.com>
2025-07-04 05:49:47 +0200

    Added the ai reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import ticketReducer from './slices/ticketSlice';
-import helpdeskReducer from './slices/helpdeskSlice';
 import aiReducer from './slices/aiSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     tickets: ticketReducer,
-    helpdesk: helpdeskReducer,
     ai: aiReducer,
   },
 }); 
